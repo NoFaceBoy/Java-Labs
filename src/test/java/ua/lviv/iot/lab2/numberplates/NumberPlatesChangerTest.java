73e741e517bee2950ea9c89211cf970f93af9b97
Oleksandr Teliuk <telyuksasha@gmail.com>
2022-05-14 00:42:11 +0300

    Added needed files and tests for lab7

---
+++ src/test/java/ua/lviv/iot/lab2/numberplates/NumberPlatesChangerTest.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.lab2.numberplates;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NumberPlatesChangerTest {
+
+	@Test
+	void testNumberPlatesChange() {
+
+	}
+}
